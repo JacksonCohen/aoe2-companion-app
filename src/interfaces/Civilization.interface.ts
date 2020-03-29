218d4046d6ea4b73952847af2971ac7e8ac6b176
Jackson <jacksonrcohen@gmail.com>
2020-03-29 18:44:50 -0400

    Adjust civ data to be proper JS format

--- src/interfaces/Civilization.interface.ts
+++ src/interfaces/Civilization.interface.ts
@@ -1,10 +1,10 @@
 export default interface Civilization {
   id: number;
   name: string;
-  expansion: string;
-  army_type: string;
-  unique_unit: string[];
-  unique_tech: string[];
+  "coat of arms": string;
+  speciality: string;
+  uniqueunit: string[];
+  "unique technologies": string[];
+  "civilization bonuses": string[];
   team_bonus: string;
-  civilization_bonus: string[];
 }
