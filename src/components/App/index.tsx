9a705cfa75b7f84f415a6a3330f9afffcbb24995
Jackson <jacksonrcohen@gmail.com>
2020-03-28 22:35:38 -0400

    Convert class components to functional

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,40 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Civilizations from "../Civilizations/";
 import Civilization from "../../interfaces/Civilization.interface";
 
-class App extends Component<{}, { civilizations: Civilization[] }> {
-  constructor(props: any) {
-    super(props);
+const App = () => {
+  const [civilizations, setCivilizations] = useState([]);
 
-    this.state = {
-      civilizations: []
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch(
       `https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations`
     )
       .then(res => res.json())
       .then(({ civilizations }) => {
-        this.setState(
-          {
-            civilizations
-          },
-          () => console.log(this.state)
-        );
+        setCivilizations(civilizations);
       });
-  }
+  });
 
-  render() {
-    return (
-      <div>
-        {this.state.civilizations && (
-          <Civilizations data={this.state.civilizations} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Civilizations data={civilizations} />
+    </div>
+  );
+};
 
 export default App;
