d6eb150370b4f87549c9bb434d2f3ce37a883779
Jackson <jacksonrcohen@gmail.com>
2020-04-09 10:44:04 -0400

    Add custom font

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const withFonts = require('nextjs-fonts');
 
 module.exports = withCSS(
-  withSass({
-    webpack: function (config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            name: '[name].[ext]',
-            esModule: false,
+  withSass(
+    withFonts({
+      webpack: function (config) {
+        config.module.rules.push({
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              name: '[name].[ext]',
+              esModule: false,
+            },
           },
-        },
-      });
-      return config;
-    },
-  })
+        });
+        return config;
+      },
+    })
+  )
 );
