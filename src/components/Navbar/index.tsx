f8e538e96eb084cb7e2f6be83f332f3a7a5b9767
Jackson <jacksonrcohen@gmail.com>
2020-04-09 19:12:10 -0400

    Implement desktop navbar design and style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,18 +8,22 @@ const Navbar = () => {
     <div className='nav'>
       <div className='nav__main'>
         <Link href='/'>
-          <a>
-            Age of Empires 2<br />
-            Companion App
+          <a className='logo'>
+            <img src='../../../static/images/logo.png' alt='Age of Empires 2 Companion App' />
           </a>
         </Link>
       </div>
 
-      <form action='/search'>
+      {/* <form action='/search'>
         <input type='text' placeholder='Enter a username' />
-      </form>
+      </form> */}
 
       <ul>
+        <li>
+          <Link href='/randomizer'>
+            <a>Randomizer</a>
+          </Link>
+        </li>
         <li>
           <Link href='/stats'>
             <a>Statistics</a>
@@ -28,6 +32,16 @@ const Navbar = () => {
         <li>
           <Link href='/strategy'>
             <a>Strategy</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/contact'>
+            <a>Contact</a>
           </Link>
         </li>
       </ul>
