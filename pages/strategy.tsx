970144d68a01d77656cb4bcf951bd4ee3a6767a6
Jackson <jacksonrcohen@gmail.com>
2020-04-15 14:10:03 -0400

    Set up basic Strategy section structure

--- pages/strategy.tsx
+++ pages/strategy.tsx
@@ -1,9 +1,11 @@
 import Navbar from '../src/components/Navbar';
+import Strategy from '../src/components/Strategy';
 import PlayerCount from '../src/components/PlayerCount';
 
 export default () => (
   <div className='strategy'>
     <Navbar active='strategy' />
+    <Strategy />
     <PlayerCount />
   </div>
 );
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface Props {
+  source: string;
+  name: string;
+}
+
+const Card = ({ source, name }: Props) => {
+  return <div className='card__container'></div>;
+};
+
+export default Card;
