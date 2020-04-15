db8d39688c1f279e90ccb4297bb5c52044e50073
Jackson <jacksonrcohen@gmail.com>
2020-04-15 14:07:27 -0400

    Set up basic Strategy section structure

---
+++ src/components/Strategy/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from '../Card';
+
+const Strategy = () => {
+  // import from db? api? data file?
+  return <div className='servce_strategy'>{/* map over data and create Cards */}</div>;
+};
+
+export default Strategy;
