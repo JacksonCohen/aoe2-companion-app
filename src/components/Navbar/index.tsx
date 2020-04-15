ab3171fc09891a7dd1b06f2edfaae3c504f2562d
Jackson <jacksonrcohen@gmail.com>
2020-04-14 20:15:54 -0400

    Optimize randomizer header css for mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import Hamburger from '../Hamburger';
 import './_Navbar.scss';
 
 const Navbar = ({ active }: { active?: string }) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const toggleHamburger = (): void => {
     setOpen(!open);
@@ -13,42 +13,43 @@ const Navbar = ({ active }: { active?: string }) => {
 
   return (
     <nav className='nav'>
-      <Hamburger isActive={open} handleClick={toggleHamburger} />
       <div className='nav__main'>
         <Link href='/'>
-          <a className='logo'>
+          <a className='nav__logo'>
             <img src='../../../static/images/logo.png' alt='Age of Empires 2 Companion App' />
           </a>
         </Link>
+
+        <div className='nav__hamburger'>
+          <Hamburger isActive={open} handleClick={toggleHamburger} />
+        </div>
       </div>
 
       {/* <form action='/search'>
         <input type='text' placeholder='Enter a username' />
       </form> */}
 
-      {open ? (
-        <div className='nav__items'>
-          <Link href='/randomizer'>
-            <a className={active === 'randomizer' ? 'active' : ''}>Randomizer</a>
-          </Link>
+      <div className={`nav__items${open ? '' : ' hidden'}`}>
+        <Link href='/randomizer'>
+          <a className={active === 'randomizer' ? 'active' : ''}>Randomizer</a>
+        </Link>
 
-          <Link href='/statistics'>
-            <a className={active === 'statistics' ? 'active' : ''}>Statistics</a>
-          </Link>
+        <Link href='/statistics'>
+          <a className={active === 'statistics' ? 'active' : ''}>Statistics</a>
+        </Link>
 
-          <Link href='/strategy'>
-            <a className={active === 'strategy' ? 'active' : ''}>Strategy</a>
-          </Link>
+        <Link href='/strategy'>
+          <a className={active === 'strategy' ? 'active' : ''}>Strategy</a>
+        </Link>
 
-          <Link href='/about'>
-            <a className={active === 'about' ? 'active' : ''}> About</a>
-          </Link>
+        <Link href='/about'>
+          <a className={active === 'about' ? 'active' : ''}> About</a>
+        </Link>
 
-          <Link href='/contact'>
-            <a className={active === 'contact' ? 'active' : ''}>Contact</a>
-          </Link>
-        </div>
-      ) : null}
+        <Link href='/contact'>
+          <a className={active === 'contact' ? 'active' : ''}>Contact</a>
+        </Link>
+      </div>
     </nav>
   );
 };
