afda95db327fa25f10b8eb6d14382375f6aa0b1a
Jackson <jacksonrcohen@gmail.com>
2020-04-16 15:04:26 -0400

    Add title and metadata for each page

--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import Navbar from '../src/components/Navbar';
 import PlayerCount from '../src/components/PlayerCount';
 
 export default () => (
-  <div className='statistics'>
-    <Navbar active='statistics' />
-    <PlayerCount />
-  </div>
+  <>
+    <Head>
+      <title>Statistics | Age of Empires 2 Companion App</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+
+    <div className='statistics'>
+      <Navbar active='statistics' />
+      <PlayerCount />
+    </div>
+  </>
 );
