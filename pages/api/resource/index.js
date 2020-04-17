e0fa61df34426b4dd6ecd500bb84cee93567717d
Jackson <jacksonrcohen@gmail.com>
2020-04-17 13:25:54 -0400

    Add type field to Resource schema

--- pages/api/resource/index.js
+++ pages/api/resource/index.js
@@ -3,12 +3,14 @@ import apiHandler from '../../../lib/api/api-handler';
 
 export default mongoMiddleware(async (req, res, connection, models) => {
   const { method } = req;
-  const { name, source } = JSON.parse(req.body);
+  // All fields in Resource schema
+  const { name, source, type } = JSON.parse(req.body);
 
   apiHandler(res, method, {
     POST: (response) => {
-      models.Resource.create({ name, source }, (error, resource) => {
+      models.Resource.create({ name, source, type }, (error, resource) => {
         if (error) {
+          console.log(error);
           connection.close();
           response.status(500).json({ error });
         } else {
