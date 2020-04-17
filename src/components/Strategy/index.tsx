546b574737aba070a53e00114be23f4aa1133054
Jackson <jacksonrcohen@gmail.com>
2020-04-17 16:44:47 -0400

    Embed videos in Strategy section

--- src/components/Strategy/index.tsx
+++ src/components/Strategy/index.tsx
@@ -3,12 +3,11 @@ import Card from '../Card';
 import GuideInterface from '../../interfaces/Guide.interface';
 
 const Strategy = ({ guides }: { guides: GuideInterface[] }) => {
-  console.log(guides);
   return (
     <div className='servce__strategy'>
-      {/* {guides.map(({ name, source, type }) => {
-        <Card name={name} source={source} type={type} />; */}
-      {/* })} */}
+      {guides.map(({ _id, name, source, type }) => {
+        return <Card key={_id} name={name} source={source} type={type} />;
+      })}
     </div>
   );
 };
