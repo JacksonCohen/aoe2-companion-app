f8143a262aedd0dd68769b6698cf7c351351935c
Jackson <jacksonrcohen@gmail.com>
2020-04-26 16:04:09 -0400

    Redirect index page to randomizer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../Navbar';
 import PlayerCount from '../PlayerCount';
+import { useRouter } from 'next/router';
 
 import '../../../sass/style.scss';
 import './_App.scss';
 
 const App = () => {
+  const Router = useRouter();
+  useEffect(() => {
+    Router.push('/randomizer', undefined, { shallow: true });
+  }, []);
+
   return (
     <>
       <Navbar />
