41fef81bab39d4a6180f8e697d26bc3c6fe7b43d
Jackson <jacksonrcohen@gmail.com>
2020-04-29 18:34:26 -0400

    Create basic form to submit profile info

--- src/components/StatisticsService/ProfileLookup/index.tsx
+++ src/components/StatisticsService/ProfileLookup/index.tsx
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProfileLookup = () => {
+import './_ProfileLookup.scss';
+interface Props {
+  setLookUpParams: Function;
+}
+
+const ProfileLookup = ({ setLookUpParams }: Props) => {
+  const [usernameInput, setUsernameInput] = useState('');
+  const [gameTypeInput, setGameTypeInput] = useState(0);
+
+  const handleChange = (event: any): void => {
+    if (event.target.name === 'username') {
+      setUsernameInput(event.target.value);
+    } else {
+      setGameTypeInput(event.target.value);
+    }
+  };
+
+  const handleSubmit = (): void => {
+    setLookUpParams({
+      username: usernameInput,
+      gameType: gameTypeInput,
+    });
+  };
+
   return (
-    <div>
-      <input type='text' name='' id='' />
-    </div>
+    // TODO: Refactor into reusable component
+    <form action='submit'>
+      <fieldset>
+        <input
+          type='text'
+          placeholder='Enter your username'
+          name='username'
+          onChange={(event) => handleChange(event)}
+        />
+      </fieldset>
+
+      <fieldset>
+        <div>
+          Select a game type
+          <div>
+            <input type='radio' id='unranked' name='game-type' value='0' onChange={handleChange} />
+            <label htmlFor='unranked'>Unranked</label>
+            <input
+              type='radio'
+              id='deathmatch'
+              name='game-type'
+              value='1'
+              onChange={handleChange}
+            />
+            <label htmlFor='deathmatch'>Deathmatch</label>
+            <input type='radio' id='tdm' name='game-type' value='2' onChange={handleChange} />
+            <label htmlFor='tdm'>Team Deathmatch</label>
+            <input
+              type='radio'
+              id='random-map'
+              name='game-type'
+              value='3'
+              onChange={handleChange}
+            />
+            <label htmlFor='random-map'>Random Map</label>
+            <input type='radio' id='trm' name='game-type' value='4' onChange={handleChange} />
+            <label htmlFor='trm'>Team Random Map</label>
+          </div>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <input type='button' name='submit' value='Submit' onClick={handleSubmit} />
+      </fieldset>
+    </form>
   );
 };
 
--- src/components/StatisticsService/Statistics/index.tsx
+++ src/components/StatisticsService/Statistics/index.tsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileLookup from '../ProfileLookup';
 
 import './_Statistics.scss';
 
 const Statistics = () => {
+  const [lookUpParams, setLookUpParams] = useState({
+    username: '',
+    gameType: '0',
+  });
+
+  useEffect(() => {
+    if (lookUpParams.username) {
+      findProfileId();
+    }
+  }, [lookUpParams]);
+
+  const findProfileId = () => {
+    const { username, gameType } = lookUpParams;
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=${gameType}&search=${username}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data, 'profile data'));
+  };
+
   return (
     <div className='service__statistics'>
-      <ProfileLookup />
+      <ProfileLookup setLookUpParams={setLookUpParams} />
     </div>
   );
 };
