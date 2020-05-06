c68023fbb4cb094e14d75072475ebfe0f841975a
Jackson <jacksonrcohen@gmail.com>
2020-05-05 23:18:24 -0400

    Add build order title

--- src/components/BuildOrderService/BuildOrder/index.tsx
+++ src/components/BuildOrderService/BuildOrder/index.tsx
@@ -40,6 +40,8 @@ const BuildOrder = ({ buildOrders }: { buildOrders: BuildOrderInterface[] }) => 
       let orderCopy = [...buildOrder];
       orderCopy[stepNum][name] = value;
       setBuildOrder(orderCopy);
+    } else if (name === 'orderTitle') {
+      setOrderTitle(value);
     } else {
       setFormData({ [name]: value });
     }
@@ -64,55 +66,80 @@ const BuildOrder = ({ buildOrders }: { buildOrders: BuildOrderInterface[] }) => 
 
   return (
     <div className='service__build-order'>
-      {buildOrder.map((step: any, index: number) => {
-        return (
-          <div className='build-order__input' key={index}>
-            {/* <input type='' value={step.image} /> */}
-            <input
-              type='text'
-              name='stepTitle'
-              maxLength={30}
-              value={step.stepTitle}
-              onChange={(event) => handleChange(event, index)}
-            />
-            <input
-              type='text'
-              name='info'
-              maxLength={40}
-              value={step.info}
-              onChange={(event) => handleChange(event, index)}
-            />
-            <span onClick={() => handleDeleteClick(index)}>&#10006;</span>
-          </div>
-        );
-      })}
-
-      <form action='submit'>
-        <fieldset>
+      {orderTitle.length === 0 ? (
+        <form className='build-order__title-input'>
           <input
             type='text'
-            name='stepTitle'
-            placeholder='Enter title'
-            maxLength={30}
-            value={formData.stepTitle}
+            name='orderTitle'
+            placeholder='Enter build order title'
+            maxLength={20}
+            value={orderTitle}
             onChange={(event) => handleChange(event)}
           />
-          <input
-            type='text'
-            name='info'
-            placeholder='Enter info'
-            maxLength={40}
-            value={formData.info}
-            onChange={(event) => handleChange(event)}
-          />
-        </fieldset>
+          <input type='button' value='Submit title' onClick={handleSubmit} />
+        </form>
+      ) : (
+        <>
+          <div className='build-order__title'>{orderTitle}</div>
 
-        <fieldset>
-          <input type='button' value='Add Step' onClick={handleAddClick} />
+          {buildOrder.map((step: any, index: number) => {
+            return (
+              <div className='build-order__input' key={index}>
+                {/* <input type='' value={step.image} /> */}
+                <input
+                  type='text'
+                  name='stepTitle'
+                  maxLength={30}
+                  value={step.stepTitle}
+                  onChange={(event) => handleChange(event, index)}
+                />
+                <input
+                  type='text'
+                  name='info'
+                  maxLength={40}
+                  value={step.info}
+                  onChange={(event) => handleChange(event, index)}
+                />
+                <span className='build-order__delete-step' onClick={() => handleDeleteClick(index)}>
+                  &#10006;
+                </span>
+              </div>
+            );
+          })}
 
-          {buildOrder.length !== 0 && <input type='button' value='Submit' onClick={handleSubmit} />}
-        </fieldset>
-      </form>
+          <form action='submit'>
+            <fieldset>
+              <input
+                type='text'
+                name='stepTitle'
+                placeholder='Enter title'
+                maxLength={30}
+                value={formData.stepTitle}
+                onChange={(event) => handleChange(event)}
+              />
+              <input
+                type='text'
+                name='info'
+                placeholder='Enter info'
+                maxLength={40}
+                value={formData.info}
+                onChange={(event) => handleChange(event)}
+              />
+              <span className='build-order__delete-step' onClick={() => setOrderTitle('')}>
+                &#10006;
+              </span>
+            </fieldset>
+
+            <fieldset>
+              <input type='button' value='Add Step' onClick={handleAddClick} />
+
+              {buildOrder.length !== 0 && (
+                <input type='button' value='Submit' onClick={handleSubmit} />
+              )}
+            </fieldset>
+          </form>
+        </>
+      )}
 
       <BuildOrderList orders={buildOrders} />
     </div>
