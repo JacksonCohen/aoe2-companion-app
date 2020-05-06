560b4cc986088086fdf98f83072c4782a556b623
Jackson <jacksonrcohen@gmail.com>
2020-05-05 22:31:55 -0400

    Add, edit, and delete steps in build order creator

--- src/components/BuildOrderService/BuildOrder/index.tsx
+++ src/components/BuildOrderService/BuildOrder/index.tsx
@@ -6,72 +6,111 @@ import './_BuildOrder.scss';
 
 interface State {
   image: string;
-  title: string;
+  stepTitle: string;
   info: string;
+  stepNum: number;
 }
 
 const BuildOrder = ({ buildOrders }: { buildOrders: BuildOrderInterface[] }) => {
   const initialState = {
     image: '',
-    title: '',
+    stepTitle: '',
     info: '',
   };
   const stateReducer = (state: State, newState: any) => ({ ...state, ...newState });
 
   const [buildOrder, setBuildOrder] = useState<any>([]);
   const [formData, setFormData] = useReducer(stateReducer, initialState);
+  const [stepNum, setStepNum] = useState(1);
+  const [orderTitle, setOrderTitle] = useState('');
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
+  const handleDeleteClick = (index: any): void => {
+    const steps = [...buildOrder];
+    steps.splice(index, 1);
+    setBuildOrder(steps);
+  };
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    stepNum?: number
+  ): void => {
     const { name, value } = event.target;
 
-    setFormData({ [name]: value });
+    if (stepNum !== undefined) {
+      let orderCopy = [...buildOrder];
+      orderCopy[stepNum][name] = value;
+      setBuildOrder(orderCopy);
+    } else {
+      setFormData({ [name]: value });
+    }
   };
 
   const handleAddClick = (): void => {
+    formData.stepNum = stepNum;
     setBuildOrder([...buildOrder, formData]);
     setFormData(initialState);
+    setStepNum(stepNum + 1);
   };
 
-  const handleSubmit = () => {
-    fetch('/api/buildOrder', {
+  const handleSubmit = (): void => {
+    fetch('/api/build-order', {
       method: 'POST',
-      body: JSON.stringify(buildOrder),
+      body: JSON.stringify({
+        title: orderTitle,
+        buildOrder,
+      }),
     });
   };
 
   return (
     <div className='service__build-order'>
-      {buildOrder.map((step: any, index: number): any => (
-        <div key={index}>
-          <img src={step.image} />
-          <div>{step.title}</div>
-          <div>{step.info}</div>
-        </div>
-      ))}
+      {buildOrder.map((step: any, index: number) => {
+        return (
+          <div className='build-order__input' key={index}>
+            {/* <input type='' value={step.image} /> */}
+            <input
+              type='text'
+              name='stepTitle'
+              maxLength={30}
+              value={step.stepTitle}
+              onChange={(event) => handleChange(event, index)}
+            />
+            <input
+              type='text'
+              name='info'
+              maxLength={40}
+              value={step.info}
+              onChange={(event) => handleChange(event, index)}
+            />
+            <span onClick={() => handleDeleteClick(index)}>&#10006;</span>
+          </div>
+        );
+      })}
 
       <form action='submit'>
         <fieldset>
           <input
             type='text'
-            name='title'
-            className='input'
+            name='stepTitle'
             placeholder='Enter title'
-            value={formData.title}
-            onChange={handleChange}
+            maxLength={30}
+            value={formData.stepTitle}
+            onChange={(event) => handleChange(event)}
           />
           <input
             type='text'
             name='info'
-            className='input'
             placeholder='Enter info'
+            maxLength={40}
             value={formData.info}
-            onChange={handleChange}
+            onChange={(event) => handleChange(event)}
           />
         </fieldset>
 
         <fieldset>
-          <input type='button' value='Add' onClick={handleAddClick} />
-          <input type='button' value='Submit' onClick={handleSubmit} />
+          <input type='button' value='Add Step' onClick={handleAddClick} />
+
+          {buildOrder.length !== 0 && <input type='button' value='Submit' onClick={handleSubmit} />}
         </fieldset>
       </form>
 
--- src/components/BuildOrderService/BuildOrderList/index.tsx
+++ src/components/BuildOrderService/BuildOrderList/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import BuildOrderInterface from '../../../interfaces/BuildOrder.interface';
 
-const BuildOrderList = ({ orders }: { orders: BuildOrderInterface[] }) => {
-  return <div></div>;
-};
+import './_BuildOrderList.scss';
+
+const BuildOrderList = ({ orders }: { orders: BuildOrderInterface[] }) => (
+  <div className='build-order__container'>
+    {orders.map(({ order }: any) => {
+      // TODO: Fix type
+      // console.log(order, 'order');
+      return (
+        <div className='build-order__item' key={order._id}>
+          <div className='build-order__title'>{order.title}</div>
+          {order.map((step: any, index: number) => (
+            <div className='build-order__step' key={index}>
+              <img src='https://vignette.wikia.nocookie.net/ageofempires/images/d/de/CastleageDE.png/revision/latest?cb=20200223093258' />
+              {/* TODO: Replace placeholder image */}
+              <div className='step__text'>
+                <div className='step__title'>{step.title}</div>
+                <div className='step__info'>{step.info}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default BuildOrderList;
